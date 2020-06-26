7b4907f609cf63e9877b9c1da72e893cad53263d
Prithivi R <63282367+Prithivi333@users.noreply.github.com>
2020-06-26 11:05:07 +0530

    Update createevent.php

--- createevent.php
+++ createevent.php
@@ -11,12 +11,11 @@ class content
 if (isset($_POST['createinvite'])){
     $n = $_POST['Ename'];
     $_SESSION['name'] = str_replace(' ', '', $n);
-    $sql="select * from events where Ename='".$name."'and Uname='".$_SESSION['Uname']."'";
+    $sql="select * from events where Ename='".$_SESSION['name']."'";
     $r=$db->query($sql);
     $name = $_SESSION['name'];
     echo "Create invites here for " . $name . "<br>";
-    
-    $sql="select * from events where Ename='".$name."'and Uname='".$_SESSION['Uname']."'";
+    $sql="select * from events where Ename='".$name."'";
     $result=$db->query($sql);
     if($result->num_rows>0){
         echo "Sorry ,but there seems to be an event with the same name.Please try with different name of event or without special characters or space."; 
@@ -28,8 +27,8 @@ if (isset($_POST['createinvite'])){
         $info->body = $_POST["body"];
         $info->footer = $_POST["footer"];
         $jinfo = json_encode($info);
-        $qry = $db->prepare("insert into events (Uname,Ename,Econtent) values (?,?,?)");
-        $qry->bind_param("sss", $uname, $name, $jinfo);
+        $qry = $db->prepare("insert into events (Uname,Ename,Econtent,Etype) values (?,?,?,?)");
+        $qry->bind_param("ssss", $uname, $name, $jinfo,$_POST['newevent']);
         $chk = $qry->execute();
         if ($chk) {
             echo "Contents added to invitation successfully!!";
@@ -44,14 +43,19 @@ if (isset($_POST['createinvite'])){
     }
 }
 ?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 <form method="post" id="inviteform" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>'>
-<label>Event:</label><input list="events" name="newevent" required class="input">
-<datalist id="events">
-    <option value="Birthday Party"></option>
-    <option value="Wedding"></option>
-    <option value="Funeral"></option>
-    <option value="Other"></option>
-</datalist>
+<label>Event:</label>
+<select id="events" name="newevent" required class="input">
+    <option value="Birthday Party">Bday party</option>
+    <option value="Wedding">Wedding/Anniversary</option>
+    <option value="Funeral">Funeral/Obituary</option>
+    <option value="Other">Other</option>
+</select>
 <label>Event Name:</label><input type="text" name="Ename" required class="input"><br>
 <label>Enter Header Text:(REQUIRED)</label><br>
     <input type="text" name="header" required class="input"><br>
@@ -61,3 +65,5 @@ if (isset($_POST['createinvite'])){
     <textarea rows="5" cols="50" name="body" form="inviteform">TEXT</textarea><br>
     <button name="createinvite">Create Invitation</button>
 </form>
+</body>
+</html>
